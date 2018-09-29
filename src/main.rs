d242920bebd16c23f2a964af714acc90a9fd4dca
David Ross <daboross@daboross.net>
2018-09-28 23:21:31 -0700

    Update to screeps-game-api v0.2

--- src/main.rs
+++ src/main.rs
@@ -119,20 +119,20 @@ fn game_loop() {
 
     if time % 32 == 3 {
         info!("running memory cleanup");
-        cleanup_memory();
+        cleanup_memory().expect("expected Memory.creeps format to be a regular memory object");
     }
 
     info!("done! cpu: {}", screeps::game::cpu::get_used())
 }
 
-fn cleanup_memory() {
+fn cleanup_memory() -> Result<(), Box<::std::error::Error>> {
     let alive_creeps: HashSet<String> = screeps::game::creeps::keys().into_iter().collect();
 
-    let screeps_memory = match screeps::memory::root().dict("creeps") {
+    let screeps_memory = match screeps::memory::root().dict("creeps")? {
         Some(v) => v,
         None => {
             warn!("not cleaning game creep memory: no Memory.creeps dict");
-            return;
+            return Ok(());
         }
     };
 
@@ -142,4 +142,6 @@ fn cleanup_memory() {
             screeps_memory.del(&mem_name);
         }
     }
+
+    Ok(())
 }
