5e58bb6248bc0ea62750d4515d15d8329823224d
Shane Madden <shanemadden@fake.email>
2021-01-22 19:04:44 -0700

    Update wasm module load to hopefully be more robust

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,37 @@
 "use strict";
+let wasm_module;
 
-Error.stackTraceLimit = Infinity;
-
-function wasm_initialize() {
-    // attempt to load the wasm only if there's enough bucket to do a bunch of work this tick
-    if (Game.cpu.bucket < 500) {
-        console.log("we are running out of time, pausing compile!" + JSON.stringify(Game.cpu));
-        return;
-    }
-    
-    // replace this initialize function on the module
-    const wasm_module = require("screeps-starter-rust");
-    // replace the export of this function with the module's
-    module.exports.loop = wasm_module.loop;
-    // run the setup function, which configures logging
-    wasm_module.setup();
-    // go ahead and run the loop for its first tick
-    wasm_module.loop();
+function console_error(...args) {
+    console.log(...args);
+    Game.notify(args.join(' '));
 }
 
-module.exports.loop = wasm_initialize;
+module.exports.loop = function() {
+    try {
+        if (wasm_module && wasm_module.__wasm) {
+            wasm_module.loop();
+        } else {
+            // attempt to load the wasm only if there's enough bucket to do a bunch of work this tick
+            if (Game.cpu.bucket < 500) {
+                console.log("we are running out of time, pausing compile!" + JSON.stringify(Game.cpu));
+                return;
+            }
+            
+            // replace this initialize function on the module
+            wasm_module = require("screeps-starter-rust");
+            // load the wasm instance!
+            wasm_module.initialize_instance();
+            // run the setup function, which configures logging
+            wasm_module.setup();
+            // go ahead and run the loop for its first tick
+            wasm_module.loop();
+        }
+    } catch(error) {
+        console_error("caught exception:", error);
+        if (error.stack) {
+            console_error("stack trace:", error.stack);
+        }
+        console_error("resetting VM next tick.");
+        wasm_module.__wasm = null;
+    }
+}
