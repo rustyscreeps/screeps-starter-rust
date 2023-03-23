4e6ac40ac6fc94142d741af7f5322d286ef0e851
Starua <starua@starua.me>
2023-03-23 11:50:40 +0800

    delete wasm module cache before reload it & place module name in to an constant

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,17 @@
 "use strict";
 let wasm_module;
 
+// replace this with the name of your module
+const MODULE_NAME = "screeps-starter-rust";
+
 function console_error(...args) {
     console.log(...args);
     Game.notify(args.join(' '));
 }
 
-module.exports.loop = function() {
+module.exports.loop = function () {
     try {
-        if (wasm_module && wasm_module.__wasm) {
+        if (wasm_module) {
             wasm_module.loop();
         } else {
             // attempt to load the wasm only if there's enough bucket to do a bunch of work this tick
@@ -16,9 +19,13 @@ module.exports.loop = function() {
                 console.log("we are running out of time, pausing compile!" + JSON.stringify(Game.cpu));
                 return;
             }
-            
-            // replace this initialize function on the module
-            wasm_module = require("screeps-starter-rust");
+
+            // delect the module from the cache, so we can reload it
+            if (MODULE_NAME in require.cache) {
+                delete require.cache[MODULE_NAME];
+            }
+            // load the wasm module
+            wasm_module = require(MODULE_NAME);
             // load the wasm instance!
             wasm_module.initialize_instance();
             // run the setup function, which configures logging
@@ -26,12 +33,12 @@ module.exports.loop = function() {
             // go ahead and run the loop for its first tick
             wasm_module.loop();
         }
-    } catch(error) {
+    } catch (error) {
         console_error("caught exception:", error);
         if (error.stack) {
             console_error("stack trace:", error.stack);
         }
         console_error("resetting VM next tick.");
-        wasm_module.__wasm = null;
+        wasm_module = null;
     }
 }
