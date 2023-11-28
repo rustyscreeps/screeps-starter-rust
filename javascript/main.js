3a4ce7750df22a222f6e21440dcae2d92a0fa1a1
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2023-11-27 19:35:27 -0500

    add console.error function for wasm_bindgen (#45)

--- javascript/main.js
+++ javascript/main.js
@@ -4,9 +4,24 @@ let wasm_module;
 // replace this with the name of your module
 const MODULE_NAME = "screeps-starter-rust";
 
-function console_error(...args) {
-    console.log(...args);
-    Game.notify(args.join(' '));
+// This provides the function `console.error` that wasm_bindgen sometimes expects to exist,
+// especially with type checks in debug mode. An alternative is to have this be `function () {}`
+// and let the exception handler log the thrown JS exceptions, but there is some additional
+// information that wasm_bindgen only passes here.
+//
+// There is nothing special about this function and it may also be used by any JS/Rust code as a convenience.
+console.error = function () {
+    const processedArgs = _.map(arguments, (arg) => {
+        if (arg instanceof Error) {
+            // On this version of Node, the `stack` property of errors contains
+            // the message as well.
+            return arg.stack;
+        } else {
+            return arg;
+        }
+    }).join(' ');
+    console.log("ERROR:", processedArgs);
+    Game.notify(processedArgs);
 }
 
 module.exports.loop = function () {
@@ -34,14 +49,14 @@ module.exports.loop = function () {
             wasm_module.loop();
         }
     } catch (error) {
-        console_error("caught exception:", error);
+        console.error("caught exception:", error);
         // we've already logged the more-descriptive stack trace from rust's panic_hook
         // if for some reason (like wasm init problems) you're not getting output from that
         // and need more information, uncomment the following:
         // if (error.stack) {
-        //     console_error("stack trace:", error.stack);
+        //     console.error("stack trace:", error.stack);
         // }
-        console_error("resetting VM next tick.");
+        console.error("resetting VM next tick.");
         wasm_module = null;
     }
 }
