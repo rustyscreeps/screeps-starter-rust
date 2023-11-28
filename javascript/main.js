6669dca7ae0d46120ef8767d9a0dbd9ab6beb2cf
Shane Madden <shanemadden@users.noreply.github.com>
2023-11-28 00:16:50 -0700

    Use halt, new logging init, fix console.error, wait for 750 cpu for init

--- javascript/main.js
+++ javascript/main.js
@@ -10,7 +10,7 @@ const MODULE_NAME = "screeps-starter-rust";
 // information that wasm_bindgen only passes here.
 //
 // There is nothing special about this function and it may also be used by any JS/Rust code as a convenience.
-console.error = function () {
+const console_error = function () {
     const processedArgs = _.map(arguments, (arg) => {
         if (arg instanceof Error) {
             // On this version of Node, the `stack` property of errors contains
@@ -24,39 +24,43 @@ console.error = function () {
     Game.notify(processedArgs);
 }
 
-module.exports.loop = function () {
-    try {
-        if (wasm_module) {
-            wasm_module.loop();
-        } else {
-            // attempt to load the wasm only if there's enough bucket to do a bunch of work this tick
-            if (Game.cpu.bucket < 500) {
-                console.log("we are running out of time, pausing compile!" + JSON.stringify(Game.cpu));
-                return;
-            }
+let halt_next_tick = false;
 
-            // delete the module from the cache, so we can reload it
-            if (MODULE_NAME in require.cache) {
-                delete require.cache[MODULE_NAME];
+module.exports.loop = function () {
+    // need to freshly override the fake console object each tick
+    console.error = console_error;
+    if (halt_next_tick === true) {
+        // we've had an error on the last tick (see error catch); skip execution during the current
+        // tick, asking to have our IVM immediately destroyed so we get a fresh environment next tick
+        // to work around https://github.com/rustwasm/wasm-bindgen/issues/3130
+        Game.cpu.halt();
+    } else {
+        try {
+            if (wasm_module) {
+                wasm_module.loop();
+            } else {
+                // attempt to load the wasm only if there's enough bucket to do a bunch of work this tick
+                if (Game.cpu.bucket < 750) {
+                    console.log("we are running out of time, pausing compile!" + JSON.stringify(Game.cpu));
+                    return;
+                }
+                // load the wasm module
+                wasm_module = require(MODULE_NAME);
+                // load the wasm instance!
+                wasm_module.initialize_instance();
+                // go ahead and run the loop for its first tick
+                wasm_module.loop();
             }
-            // load the wasm module
-            wasm_module = require(MODULE_NAME);
-            // load the wasm instance!
-            wasm_module.initialize_instance();
-            // run the setup function, which configures logging
-            wasm_module.setup();
-            // go ahead and run the loop for its first tick
-            wasm_module.loop();
+        } catch (error) {
+            console.error("caught exception:", error);
+            // we've already logged the more-descriptive stack trace from rust's panic_hook
+            // if for some reason (like wasm init problems) you're not getting output from that
+            // and need more information, uncomment the following:
+            // if (error.stack) {
+            //     console.error("stack trace:", error.stack);
+            // }
+            console.error("resetting VM next tick.");
+            halt_next_tick = true;
         }
-    } catch (error) {
-        console.error("caught exception:", error);
-        // we've already logged the more-descriptive stack trace from rust's panic_hook
-        // if for some reason (like wasm init problems) you're not getting output from that
-        // and need more information, uncomment the following:
-        // if (error.stack) {
-        //     console.error("stack trace:", error.stack);
-        // }
-        console.error("resetting VM next tick.");
-        wasm_module = null;
     }
 }
