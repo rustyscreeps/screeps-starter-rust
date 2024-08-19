b32c1461057ec20aff6947bf7bc6e57c4eb0c3e2
Shane Madden <shanemadden@users.noreply.github.com>
2024-08-19 14:37:50 -0600

    deploy script improvements from private bot

--- js_tools/deploy.js
+++ js_tools/deploy.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const fsExtra = require('fs-extra');
 const path = require('path');
-const spawnSync = require('child_process').spawnSync;
+const { spawnSync } = require('child_process');
 
 const { rollup } = require('rollup');
 const babel = require('@rollup/plugin-babel');
@@ -23,15 +24,24 @@ const argv = require('yargs')
   })
   .argv;
 
+// load configuration from .screeps.yaml
+// unified config format:
+// https://github.com/screepers/screepers-standards/blob/master/SS3-Unified_Credentials_File.md
 function load_config() {
   const yaml_conf = yaml.parse(fs.readFileSync('.screeps.yaml', { encoding: 'utf8' }));
-
-  const branch = yaml_conf.servers[argv.server].branch || 'default';
-  let use_terser = false;
-  let extra_options = [];
-
   const configs = yaml_conf.configs || {};
 
+  if (!yaml_conf.servers[argv.server]) {
+    console.log(`no configuration found for server ${argv.server} in .screeps.yaml`);
+    return
+  }
+
+  const branch = yaml_conf.servers[argv.server].branch || 'default';
+
+  // whether the terser minification step should be called during rollup
+  // read the default from the '*' key first (if it exists) then override
+  // with server config (if it exists)
+  let use_terser = false;
   const terser_configs = configs.terser || {};
   if (terser_configs['*'] !== undefined) {
     use_terser = terser_configs['*'];
@@ -40,6 +50,9 @@ function load_config() {
     use_terser = terser_configs[argv.server];
   }
 
+  // extra options to pass to wasm-pack - append the options from the '*'
+  // key then any server-specific options
+  let extra_options = [];
   const wasm_pack_options = configs['wasm-pack-options'] || {};
   if (wasm_pack_options['*']) {
     extra_options = extra_options.concat(wasm_pack_options['*'])
@@ -48,26 +61,23 @@ function load_config() {
     extra_options = extra_options.concat(wasm_pack_options[argv.server])
   }
 
-  return {
-    branch: branch,
-    use_terser: use_terser,
-    extra_options: extra_options,
-  }
+  return { branch, use_terser, extra_options }
 }
 
-function output_clean() {
+// clear the dist and pkg directories of any existing build results
+async function output_clean() {
   for (dir of ['dist', 'pkg']) {
-    if (fs.existsSync(dir)) {
-      fs.rmSync(dir, { recursive: true });
-    }
+    await fsExtra.emptyDir(dir);
   }
 }
 
+// invoke wasm-pack, compiling the wasm module into the pkg directory
 function run_wasm_pack(extra_options) {
   let args = ['run', 'nightly', 'wasm-pack', 'build', '--target', 'web', '--release', '.', ...extra_options];
   spawnSync('rustup', args, { stdio: 'inherit' })
 }
 
+// run the rollup bundler on the main.js file, outputting the results to the dist directory
 async function run_rollup(use_terser) {
   const bundle = await rollup({
     input: 'js_src/main.js',
@@ -94,20 +104,22 @@ async function run_rollup(use_terser) {
   });
 }
 
-async function upload(server, branch, dryrun) {
+// load the built code from the dist directory and craft it into the format the API needs
+function load_built_code() {
   let modules = {};
+  // track how much space our code uses, since that's limited to 5 MiB
   let used_bytes = 0;
 
-  await fs.readdirSync('dist').map(function (filename) {
+  fs.readdirSync('dist').map(filename => {
     if (filename.endsWith('.wasm')) {
-      const data = fs.readFileSync(path.join('dist', filename), {encoding: 'base64'});
+      const data = fs.readFileSync(path.join('dist', filename), { encoding: 'base64' });
       const filename_stripped = filename.replace(/\.wasm$/, '');
       used_bytes += data.length;
       modules[filename_stripped] = {
         binary: data,
       }
     } else {
-      const data = fs.readFileSync(path.join('dist', filename), {encoding: 'utf8'});
+      const data = fs.readFileSync(path.join('dist', filename), { encoding: 'utf8' });
       const filename_stripped = filename.replace(/\.js$/, '');
       used_bytes += data.length;
       modules[filename_stripped] = data;
@@ -117,31 +129,33 @@ async function upload(server, branch, dryrun) {
   const used_mib = used_bytes / (1024 * 1024);
   const used_percent = 100 * used_mib / 5;
 
-  const usage_string = `${used_mib.toFixed(2)} MiB of 5.0 MiB code size limit (${used_percent.toFixed(2)}%)`
+  return { used_mib, used_percent, modules }
+}
+
+// upload the code to the servers using the API (or simulate it without uploading, if
+// dryrun is true)
+async function upload(code, server, branch, dryrun) {
+  const usage_string = `${code.used_mib.toFixed(2)} MiB of 5.0 MiB code size limit (${code.used_percent.toFixed(2)}%)`
   if (dryrun) {
     console.log(`Not uploading due to --dryrun; would use ${usage_string}`);
   } else {
-    console.log(`Uploading; using ${usage_string}`);
+    console.log(`Uploading to branch ${branch}; using ${usage_string}`);
     const api = await ScreepsAPI.fromConfig(server);
-    const response = await api.code.set(branch, modules);
+    const response = await api.code.set(branch, code.modules);
     console.log(JSON.stringify(response));
   }
 }
 
 async function run() {
   const config = load_config();
-
-  // clean output (pkg/dist dirs)
-  output_clean();
-  
-  // run cargo build
-  run_wasm_pack(config.extra_options);
-
-  // run rollup
+  if (!config) {
+    return
+  }
+  await output_clean();
+  await run_wasm_pack(config.extra_options);
   await run_rollup(config.use_terser);
-
-  // read resulting files and upload
-  await upload(argv.server, config.branch, argv.dryrun);
+  const code = load_built_code();
+  await upload(code, argv.server, config.branch, argv.dryrun);
 }
 
 run().catch(console.error)
